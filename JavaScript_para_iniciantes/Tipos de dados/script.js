5614b46a0bc5daf12f6928aa7020a25b8f099000
Bruno Purper Farias <brunoplusultra@outlook.com>
2021-01-18 13:06:27 -0300

    Gostando muito das aulas pqp

---
+++ JavaScript_para_iniciantes/Tipos de dados/script.js
@@ -0,0 +1,35 @@
+// utilizar o typeof para vereficicar o tipo de dado da variável!
+
+var nome = 'Bruno';
+var idade = 22;
+var time = null;
+var simbolo = Symbol();
+console.log(typeof simbolo);
+
+// Podemos somar String's -> Concatenação
+
+var nome = 'Bruno';
+var sobrenome = 'Purper';
+var nomeCompleto = nome + ' ' + sobrenome;
+
+console.log(nomeCompleto);
+
+// Podemos somar números com String's
+
+var gols = 1000;
+var frase = 'Romario fez ' + gols + ' gols';
+
+//  Temos como ignorar o atributo do caracter utilizando -> "\" e a frase entre `--`
+
+var melhor = 'teste';
+
+var frase1 = 'Este é o \"melhor\" jogo';
+
+// TEMPLATE STRING  `${var}`
+
+var gols = 1000;
+var frase = 'Romario fez ' + gols + ' gols';
+var frase2 = `Romario fez ${gols * 2} gols`; // Posso até mesmo fazer operações aritiméticas com a variável.
+
+console.log(frase2);
+
---
+++ JavaScript_para_iniciantes/Variavies/script.js
@@ -0,0 +1,32 @@
+
+const possuiFaculdade = false;
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+// Tem como criarmos várias variáveis de uma vez só, utilizando somente a virgula.
+
+var sobrenome = "Purper",
+  cidade = "Cachoeirinha";
+
+// Temos como decalarar variáveis sem valor dentro. (Retorna Undefined)
+
+var semDefinir;
+
+
+// const time = 'Vasco';
+// time = 'Falemngo';
+// console.log(time);
+
+// Exercício 
+
+var nome = "Bruno Purper";
+var idade = 22;
+var comidaFavorita;
+comidaFavorita = 'Pizza';
+variavelSemValor1;
+variavelSemValor2;
+variavelSemValor3;
+variavelSemValor4;
+variavelSemValor5;
