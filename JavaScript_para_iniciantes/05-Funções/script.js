b71f473fe6724396b7662b7a6801c59fd54f246e
Bruno Purper Farias <brunoplusultra@outlook.com>
2021-02-15 02:58:01 -0300

    Começando o estudo de funções

---
+++ JavaScript_para_iniciantes/05-Funções/script.js
@@ -0,0 +1,17 @@
+function areaQuadrado(lado) {
+
+    return lado * lado;
+}
+
+console.log(areaQuadrado(10)); 
+
+// ============= * =====================
+
+function pi() {
+  return 3.14;
+}
+
+var total = 5 * pi();
+console.log(total);
+
+// ============= * =====================
