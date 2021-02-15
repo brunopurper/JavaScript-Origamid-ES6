b20d208cafcb6b681e2671c9ddb5131c48c3550c
Bruno Purper Farias <brunoplusultra@outlook.com>
2021-02-15 05:00:44 -0300

    Update script.js

--- JavaScript_para_iniciantes/05-Funções/script.js
+++ JavaScript_para_iniciantes/05-Funções/script.js
@@ -15,3 +15,40 @@ var total = 5 * pi();
 console.log(total);
 
 // ============= * =====================
+
+function imc(peso, altura) {
+  const imc = peso / (altura * altura);
+  return imc;
+
+}
+
+console.log(imc(80, 1.8));
+
+// ============= * =====================
+
+function corFavorita(cor) {
+
+    if(cor == "Azul"){
+      return 'Eu gosto do céu';
+
+    }else if(cor == "Verde") {
+      return 'Eu gosto do Gon';
+    }else{
+      return 'Eu não gosto de cores.';
+    }
+}
+// ============= * =====================
+addEventListener('click', function(){ 
+  console.log('Oi');
+})
+
+// ============= * =====================
+function imc2(peso, altura) {
+
+  const imc = peso / (altura * altura);
+  console.log(imc);
+}
+
+console.log(imc2(80, 1.8)); // retorna o imc e undefined pq n tem o return
+
+// ============= * =====================
