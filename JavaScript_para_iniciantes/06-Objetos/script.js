3625d325cf5e21035657a19714e33930a92a793e
Bruno Purper Farias <brunoplusultra@outlook.com>
2021-03-01 00:22:37 -0300

    Update script.js

--- JavaScript_para_iniciantes/06-Objetos/script.js
+++ JavaScript_para_iniciantes/06-Objetos/script.js
@@ -7,3 +7,49 @@ var pessoa = {
 
 pessoa.nome; // 'André'
 pessoa.possuiFaculdade; // true
+
+// ====================================================
+
+var quadrado = {
+  lados: 4,
+  area: function (lado) {
+    return lado * lado;
+  },
+  perimetro: function (lado) {
+    return this.lados * lado;
+  },
+}
+
+quadrado.lados; // 4
+quadrado.area(5); // 25
+quadrado.perimetro(5); // 20
+
+
+// Crie um objeto com os seus dados pessoais
+// Deve possui pelo menos duas propriedades nome e sobrenome
+
+var nomeCompleto = {
+  nome: 'Bruno',
+  sobreNome: 'Purper',
+}
+// Crie um método no objeto anterior, que mostre o seu nome completo
+nomeCompleto.nome.sobreNome;
+
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+  preco: 1000,
+  portas: 4,
+  marca: 'Audi',
+}
+
+carro.preco = 3000;
+
+// Crie um objeto de um cachorro que represente um labrador,
+// preto com 10 anos, que late ao ver um homem
+
+var cachorro = {
+  raca: 'Labrador',
+  cor: 'Preta',
+  idade: 10,
+  caracteristica: "Late ao ver um homem"
+}
